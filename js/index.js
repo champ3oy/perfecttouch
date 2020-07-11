0993c130ab88dd5a9156014871922da28844505d
samarithan <akotosel6@gmail.com>
2020-07-11 10:26:22 +0000

    bar dynamic

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 let bar = document.getElementById("bar");
 
 bar.addEventListener("click", () => {
-  document.getElementById('menu').classList.add("show");
+  if (document.getElementById('menu').classList.contains('show')) {
+    document.getElementById('menu').classList.remove("show");
+  document.getElementById('icon').setAttribute('src', './image/menu.svg')
+  } else {
+    document.getElementById('menu').classList.add("show");
+  document.getElementById('icon').setAttribute('src', './image/x.svg')
+  }
+  
 });
